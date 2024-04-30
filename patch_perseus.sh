297b599d98278df0cd9327a66f59e5c106064571
hyl1999 <35450652+hyl1999@users.noreply.github.com>
2024-04-30 11:42:25 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -29,7 +29,7 @@ if [ ! -f "com.bilibili.AzurLane.apk" ]; then
 
     # eg: wget "your download link" -O "your packge name.apk" -q
     #if you want to patch .xapk, change the suffix here to wget "your download link" -O "your packge name.xapk" -q
-    wget https://pkg.biligame.com/games/blhx_7.1.1_20230610_1_20230616_031339_073b2.apk -O com.bilibili.AzurLane.apk -q
+    wget http://cww521.top:5244/d/%E5%AE%B6%E5%BA%AD/1/%E7%A2%A7%E8%93%9D%E8%88%AA%E7%BA%BF/%E6%B8%B8%E6%88%8F%E6%96%87%E4%BB%B6/b%E6%9C%8D.apk?sign=pYHdhc5YBI-F0v9JRGo29cmwnXvMvv400c3hRPjaIjE=:0 -O com.bilibili.AzurLane.apk -q
     echo "apk downloaded !"
     
     # if you can only download .xapk file uncomment 2 lines below. (delete the '#')
